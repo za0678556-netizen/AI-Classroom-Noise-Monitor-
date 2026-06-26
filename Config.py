dfb98132693b36df2a6d506929ad2b1e43aae84c
za0678556-netizen <za0678556@gmail.com>
2026-06-26 22:56:54 +0500

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,6 @@
+# config.py
+THRESHOLD_QUIET = 50      # 50dB se kam = Quiet
+THRESHOLD_NOISY = 70      # 70dB se zyada = Noisy
+SAMPLE_RATE = 44100       # Microphone speed
+CHUNK_SIZE = 1024         # Data size
+LOG_FILE = "noise_log.json"
